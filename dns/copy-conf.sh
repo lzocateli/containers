60aa63d1523463979e706512e1bb9a94bfc787eb
brazildevops <devops@nuuve.com.br>
2024-08-25 02:03:16 -0300

    scripts

--- dns/copy-conf.sh
+++ dns/copy-conf.sh
@@ -4,10 +4,10 @@
        cp ./pihole/base/99-edns.conf /userapps/vol-pihole/etc/pihole/
        chown brazildevops:brazildevops /userapps/vol-pihole/etc/pihole/99-edns.conf
    fi
-   if [ ! -f "/userapps/vol-pihole/etc/unbound/unbound.conf" ]; then
+#   if [ ! -f "/userapps/vol-pihole/etc/unbound/unbound.conf" ]; then
        cp ./unbound/unbound.conf /userapps/vol-pihole/etc/unbound/
        chown brazildevops:brazildevops /userapps/vol-pihole/etc/unbound/unbound.conf
-   fi
+#   fi
 
 touch /userapps/var/log/unbound/unbound.log
 chown brazildevops:brazildevops /userapps/var/log/unbound/unbound.log
