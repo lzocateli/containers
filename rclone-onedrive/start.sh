ca442dcbb4b38ac0358d8c6f4bbb2975171857f0
Lincoln Zocateli <lzocateli@outlook.com>
2025-03-19 23:08:21 -0300

    Update start.sh

--- rclone-onedrive/start.sh
+++ rclone-onedrive/start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo "Running backup on start..."
 /userapps/utils/backup.sh 2>&1 | tee /var/log/${LOG_FILE_NAME}
-echo "Backup on start done"
 
-echo "Starting cron backups"
+echo "Iniciando servico de CRON"
 crond -f -d 8
