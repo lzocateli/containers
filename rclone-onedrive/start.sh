b187722147b5d14f2b86bbe3642183e1ac3b05f1
Lincoln Zocateli <lzocateli@outlook.com>
2025-03-23 00:52:36 -0300

    Update start.sh

--- rclone-onedrive/start.sh
+++ rclone-onedrive/start.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
 echo "Gerando log em: ${LOG_FILE_NAME}"
-/userapps/utils/backup.sh 2>&1 | tee /var/log/${LOG_FILE_NAME}
+##/userapps/utils/backup.sh 2>&1 | tee /var/log/${LOG_FILE_NAME}
+
+echo "Validando cronjobs..."
+if crontab -l > /dev/null 2>&1; then
+    echo "Arquivo cron válido."
+else
+    echo "Erro na configuração do cronjobs!" >&2
+    exit 1
+fi
+
+echo "Executando validação manual do script..."
+if sh /userapps/utils/backup.sh; then
+    echo "Script backup.sh executado com sucesso."
+else
+    echo "Erro ao executar backup.sh!" >&2
+    exit 1
+fi
+
 
 if [ ! -f /etc/crontabs/root ]; then
     echo "Erro: Arquivo cronjobs não encontrado!" >&2
