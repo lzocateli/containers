86bf3e885194761c898ae73b9836afd2e5b64ccc
Lincoln Zocateli <lzocateli@outlook.com>
2025-03-23 00:49:17 -0300

    Update start.sh

--- rclone-onedrive/start.sh
+++ rclone-onedrive/start.sh
@@ -3,6 +3,19 @@
 echo "Gerando log em: ${LOG_FILE_NAME}"
 /userapps/utils/backup.sh 2>&1 | tee /var/log/${LOG_FILE_NAME}
 
-echo "Iniciado servico de CRON"
-crond -f -d 8
+if [ ! -f /etc/crontabs/root ]; then
+    echo "Erro: Arquivo cronjobs não encontrado!" >&2
+    exit 1
+else
+    echo "Iniciando CRON..."
+fi
+
+crond -f &
+
+if [ $? -ne 0 ]; then
+    echo "Erro ao iniciar o serviço cron!" >&2
+    exit 1
+fi
+echo "Cron inicializado com sucesso."
+
 python3 /userapps/utils/proxima_execucao.py
